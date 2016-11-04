e0dfc335a81925915f113f5316196cd28f881225
Antonio Armero <antonioarmeroperez@gmail.com>
2016-11-04 13:26:17 +0100

    Multiples Preguntas

--- models/models.js
+++ models/models.js
@@ -23,6 +23,10 @@ sequelize.sync().then(function() {
 					pregunta: 'Capital de Italia',
 					respuesta: 'Roma'
 				})
+			Quiz.create({
+					pregunta: 'Capital de Portugal',
+					respuesta: 'Lisboa'
+				})
 				.then(function() {
 					console.log('Base de Datos inicializada');
 				});
