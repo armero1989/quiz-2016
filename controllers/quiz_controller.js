c06a231685144312b9fb28955a78f74d732de468
Antonio Armero <antonioarmeroperez@gmail.com>
2016-11-08 09:34:27 +0100

    Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,22 @@
 var models =require('../models/models.js');
 
+//GET /quizes/new
+exports.new=function (req,res) {
+	var quiz=models.Quiz.build(//crea objeto quiz
+		{pregunta:"Pregunta",respuesta:"Respuesta"}
+		);
+		res.render('quizes/new',{quiz:quiz});
+};
+
+//POST /quizes/create
+exports.create=function (req,res) {
+	var quiz=models.Quiz.build(req.body.quiz);
+	//guarda en DB los campos pregunta y respuesrta de quiz
+	quiz.save({fields:["pregunta","respuesta"]}).then(function() {
+		res.redirect('/quizes');
+
+	});  //Redirecion HTTP (url relativo) lista de preguntas
+}
 //Autoload -factoriza el codigo si la ruta incluye :quizId
 exports.load=function (req,res,next,quizId) {
 	models.Quiz.findById(quizId).then(
