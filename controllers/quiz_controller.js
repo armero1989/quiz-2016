c2298d967a081b7778fc780fffe13f442ca966fe
Antonio Armero <antonioarmeroperez@gmail.com>
2016-11-15 18:32:36 +0100

    Editar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,69 +1,125 @@
-var models =require('../models/models.js');
+var models = require('../models/models.js');
 
 //GET /quizes/new
-exports.new=function (req,res) {
-	var quiz=models.Quiz.build(//crea objeto quiz
-		{pregunta:"Pregunta",respuesta:"Respuesta"}
-		);
-		res.render('quizes/new',{quiz:quiz,errors:[]});
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{
+			pregunta: "Pregunta",
+			respuesta: "Respuesta"
+		}
+	);
+	res.render('quizes/new', {
+		quiz: quiz,
+		errors: []
+	});
 };
 
 //POST /quizes/create
-exports.create=function (req,res) {
-	var quiz=models.Quiz.build(req.body.quiz);
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
 	quiz
-	.validate()
-	.then(
-		function (err) {
-			if(err){
-				res.render('quizes/new',{quiz:quiz,errors:err.errors});
-			}else{
-		
-	//guarda en DB los campos pregunta y respuesrta de quiz
-	quiz.save({fields:["pregunta","respuesta"]}).then(function() {
-		res.redirect('/quizes');});  //Redirecion HTTP (url relativo) lista de preguntas
-}
-});
+		.validate()
+		.then(
+			function(err) {
+				if (err) {
+					res.render('quizes/new', {
+						quiz: quiz,
+						errors: err.errors
+					});
+				} else {
+
+					//guarda en DB los campos pregunta y respuesrta de quiz
+					quiz.save({
+						fields: ["pregunta", "respuesta"]
+					}).then(function() {
+						res.redirect('/quizes');
+					}); //Redirecion HTTP (url relativo) lista de preguntas
+				}
+			});
 
 };
+//GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload de instancia de quiz
+	res.render('quizes/edit', {
+		quiz: quiz,
+		errors: []
+	});
+};
+//PUT /quizes/:id
+exports.update = function(req, res) {
+		req.quiz.pregunta = req.body.quiz.pregunta;
+		req.quiz.respuesta = req.body.quiz.respuesta;
 
-//Autoload -factoriza el codigo si la ruta incluye :quizId
-exports.load=function (req,res,next,quizId) {
-	models.Quiz.findById(quizId).then(
-		function (quiz) {
-		if(quiz){
-			req.quiz=quiz;
-			next();
-		}else{
-			next(new Error ('No existe quizId = '+quizId));}
+		req.quiz
+			.validate()
+			.then(
+				function(err) {
+					if (err) {
+						res.render('quizes/edit', {
+							quiz: req.quiz,
+							errors: err.errors
+						});
+					} else {
+						req.quiz
+							.save({
+								fields: ["pregunta", "respuesta"]
+							})
+							.then(function() {
+								res.redirect('/quizes');
+							});
+					}
+				});
 	}
-	).catch(function(error){
+	//Autoload -factoriza el codigo si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe quizId = ' + quizId));
+			}
+		}
+	).catch(function(error) {
 		next(error);
 	});
 };
 
 
 //GET /quizes
-exports.index=function(req,res){
-	models.Quiz.findAll().then(function (quizes) {
-		res.render('quizes/index',{quizes:quizes,errors:[]});
-	}
-	).catch(function(error){next(error);})
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index', {
+			quizes: quizes,
+			errors: []
+		});
+	}).catch(function(error) {
+		next(error);
+	})
 
 };
 //GET /quizes/:id
 
-exports.show=function (req,res) {
+exports.show = function(req, res) {
 
-	res.render('quizes/show',{quiz: req.quiz,errors:[]});
+	res.render('quizes/show', {
+		quiz: req.quiz,
+		errors: []
+	});
 };
 
 //GET /quizes/answer
 
-exports.answer=function (req,res) {
-	var resultado="Incorrecto";
-	if(req.query.respuesta===req.quiz.respuesta){
-		resultado="Correcto";
+exports.answer = function(req, res) {
+	var resultado = "Incorrecto";
+	if (req.query.respuesta === req.quiz.respuesta) {
+		resultado = "Correcto";
 	}
-	res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado,errors:[]});
+	res.render('quizes/answer', {
+		quiz: req.quiz,
+		respuesta: resultado,
+		errors: []
+	});
 };
