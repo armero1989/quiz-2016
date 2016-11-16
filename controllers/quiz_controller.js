abb54f5928b2334754c6d007bf455a62d4469404
Antonio Armero <antonioarmeroperez@gmail.com>
2016-11-16 19:34:12 +0100

    Crear Comentarios A

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -82,7 +82,10 @@ exports.destroy=function (req,res) {
 };
 	//Autoload -factoriza el codigo si la ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.findById(quizId).then(
+	models.Quiz.findOne({
+		where:{id:Number(quizId)},
+		include:[{model:models.Comment}]
+	}).then(
 		function(quiz) {
 			if (quiz) {
 				req.quiz = quiz;
--- models/models.js
+++ models/models.js
@@ -13,7 +13,13 @@ var sequelize = new Sequelize(null, null, null, {
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
+//Importar definicion tabla Comment
+var Comment=sequelize.import(path.join(__dirname,'comment'));
 
+exports.Comment=Comment;//exportar definicion de tabla Comment
+//Relaciones
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
 	//then(..) ejecuta el manejador una vez creada la tabla
